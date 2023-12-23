9e9820afecb4c9e7bffb327aeb1e8a5bec10c39c
soomtochukwu <onwuajuesesomtochukwu@gmail..com>
2023-12-23 19:37:51 +0100

    classified

--- front_end/app/about/page.tsx
+++ front_end/app/about/page.tsx
@@ -1,71 +1,208 @@
 import "./page.css"
 export default function about() {
     return (
-        <p className="bg-green-200 py-4 border-2 rounded-2xl p-3 max-w-4xl mx-auto">
-            dBUNN - BUNN DApp MVP for Decentralized Autonomous Organizations (DAOs)
-            <br />
-            Key Technologies:
-            <br />
-            Blockchain Platform: Ethereum for smart contract deployment. Wallet
-            Integration: MetaMask for user authentication and transactions. Ether.js:
-            For interaction between the DApp and the blockchain. Frontend Framework:
-            Next.js for the user interface. Smart Contract Language: Solidity. Token
-            Standard: ERC-20 for governance tokens.
-            <br />
-            Milestones:
-            <br />
-            1. Project Initiation (1 week: Nov 4 - 10): Define project scope and
-            goals. Assemble the development team. Select the blockchain platform and
-            technologies.
-            <br />
-            2. DAO Creation and Membership Management (1 week: Nov 11 - 18 ): Develop
-            DAO creation functionality. Implement member management, allowing users to
-            join or leave DAOs.
-            <br />
-            3. User Authentication and Wallet Integration (1 week: ):
-            <br />
-            Secure user authentication with MetaMask. Enable seamless wallet
-            integration for transactions.
-            <br />
-            4. Proposal Management (1 week):
-            <br />
-            Implement a basic system for users to submit proposals. Allow for proposal
-            tracking and visibility.
-            <br />
-            5. Voting System (1 week): Create a simple, transparent voting mechanism.
-            Implement a basic majority voting system.
-            <br />
-            6. Smart Contract Development (1 week): Develop and deploy smart contracts
-            for DAO governance. Ensure basic security and auditability.
-            <br />
-            7. Token Integration (1 week): Create or integrate a governance token
-            (ERC-20). Implement basic token transfer functionality.
-            <br />
-            8. UI/UX Design (1 week):
-            <br />
-            Design a basic user interface for managing DAO memberships and voting
-            history. Focus on usability and simplicity.
-            <br />
-            9. Testing and Bug Fixing (1 week): Thoroughly test the DApp to identify
-            and fix critical bugs. Conduct limited user testing.
-            <br />
-            10. Deployment (1 week): Deploy the DApp on a testnet for initial testing.
-            Prepare for mainnet deployment.
-            <br />
-            11. Documentation (1 week): Provide minimal documentation for users and
-            developers. Include essential user guides and contract descriptions.
-            <br />
-            12. Compliance and Legal Assessment (1 week): Ensure basic compliance with
-            local regulations and legal requirements.
-            <br />
-            13. Maintenance/Bug fixing: Maintenance through Smart contract
-            upgradability.
-            <br />
-            Signed
-            <br />Favor Lawrence
-            <br />Somtochukwu Kelechi Onwuajuese
-            <br />Franklin Ugwuoke
-            <br />
-        </p>
+        <>
+            <p className="bg-green-200 py-4 border-2 rounded-2xl p-3 max-w-4xl mx-auto">
+                dBUNN - BUNN DApp MVP for Decentralized Autonomous Organizations (DAOs)
+                <br />
+                Key Technologies:
+                <br />
+                Blockchain Platform: Ethereum for smart contract deployment. Wallet
+                Integration: MetaMask for user authentication and transactions. Ether.js:
+                For interaction between the DApp and the blockchain. Frontend Framework:
+                Next.js for the user interface. Smart Contract Language: Solidity. Token
+                Standard: ERC-20 for governance tokens.
+                <br />
+                Milestones:
+                <br />
+                1. Project Initiation (1 week: Nov 4 - 10): Define project scope and
+                goals. Assemble the development team. Select the blockchain platform and
+                technologies.
+                <br />
+                2. DAO Creation and Membership Management (1 week: Nov 11 - 18 ): Develop
+                DAO creation functionality. Implement member management, allowing users to
+                join or leave DAOs.
+                <br />
+                3. User Authentication and Wallet Integration (1 week: ):
+                <br />
+                Secure user authentication with MetaMask. Enable seamless wallet
+                integration for transactions.
+                <br />
+                4. Proposal Management (1 week):
+                <br />
+                Implement a basic system for users to submit proposals. Allow for proposal
+                tracking and visibility.
+                <br />
+                5. Voting System (1 week): Create a simple, transparent voting mechanism.
+                Implement a basic majority voting system.
+                <br />
+                6. Smart Contract Development (1 week): Develop and deploy smart contracts
+                for DAO governance. Ensure basic security and auditability.
+                <br />
+                7. Token Integration (1 week): Create or integrate a governance token
+                (ERC-20). Implement basic token transfer functionality.
+                <br />
+                8. UI/UX Design (1 week):
+                <br />
+                Design a basic user interface for managing DAO memberships and voting
+                history. Focus on usability and simplicity.
+                <br />
+                9. Testing and Bug Fixing (1 week): Thoroughly test the DApp to identify
+                and fix critical bugs. Conduct limited user testing.
+                <br />
+                10. Deployment (1 week): Deploy the DApp on a testnet for initial testing.
+                Prepare for mainnet deployment.
+                <br />
+                11. Documentation (1 week): Provide minimal documentation for users and
+                developers. Include essential user guides and contract descriptions.
+                <br />
+                12. Compliance and Legal Assessment (1 week): Ensure basic compliance with
+                local regulations and legal requirements.
+                <br />
+                13. Maintenance/Bug fixing: Maintenance through Smart contract
+                upgradability.
+                <br />
+                Signed
+                <br />Favor Lawrence
+                <br />Somtochukwu Kelechi Onwuajuese
+                <br />Franklin Ugwuoke
+                <br />
+            </p>
+
+
+            <p className="bg-green-200 py-4 border-2 rounded-2xl p-3 max-w-4xl mx-auto">
+                dBUNN - BUNN DApp MVP for Decentralized Autonomous Organizations (DAOs)
+                <br />
+                Key Technologies:
+                <br />
+                Blockchain Platform: Ethereum for smart contract deployment. Wallet
+                Integration: MetaMask for user authentication and transactions. Ether.js:
+                For interaction between the DApp and the blockchain. Frontend Framework:
+                Next.js for the user interface. Smart Contract Language: Solidity. Token
+                Standard: ERC-20 for governance tokens.
+                <br />
+                Milestones:
+                <br />
+                1. Project Initiation (1 week: Nov 4 - 10): Define project scope and
+                goals. Assemble the development team. Select the blockchain platform and
+                technologies.
+                <br />
+                2. DAO Creation and Membership Management (1 week: Nov 11 - 18 ): Develop
+                DAO creation functionality. Implement member management, allowing users to
+                join or leave DAOs.
+                <br />
+                3. User Authentication and Wallet Integration (1 week: ):
+                <br />
+                Secure user authentication with MetaMask. Enable seamless wallet
+                integration for transactions.
+                <br />
+                4. Proposal Management (1 week):
+                <br />
+                Implement a basic system for users to submit proposals. Allow for proposal
+                tracking and visibility.
+                <br />
+                5. Voting System (1 week): Create a simple, transparent voting mechanism.
+                Implement a basic majority voting system.
+                <br />
+                6. Smart Contract Development (1 week): Develop and deploy smart contracts
+                for DAO governance. Ensure basic security and auditability.
+                <br />
+                7. Token Integration (1 week): Create or integrate a governance token
+                (ERC-20). Implement basic token transfer functionality.
+                <br />
+                8. UI/UX Design (1 week):
+                <br />
+                Design a basic user interface for managing DAO memberships and voting
+                history. Focus on usability and simplicity.
+                <br />
+                9. Testing and Bug Fixing (1 week): Thoroughly test the DApp to identify
+                and fix critical bugs. Conduct limited user testing.
+                <br />
+                10. Deployment (1 week): Deploy the DApp on a testnet for initial testing.
+                Prepare for mainnet deployment.
+                <br />
+                11. Documentation (1 week): Provide minimal documentation for users and
+                developers. Include essential user guides and contract descriptions.
+                <br />
+                12. Compliance and Legal Assessment (1 week): Ensure basic compliance with
+                local regulations and legal requirements.
+                <br />
+                13. Maintenance/Bug fixing: Maintenance through Smart contract
+                upgradability.
+                <br />
+                Signed
+                <br />Favor Lawrence
+                <br />Somtochukwu Kelechi Onwuajuese
+                <br />Franklin Ugwuoke
+                <br />
+            </p>
+
+            <p className="bg-green-200 py-4 border-2 rounded-2xl p-3 max-w-4xl mx-auto">
+                dBUNN - BUNN DApp MVP for Decentralized Autonomous Organizations (DAOs)
+                <br />
+                Key Technologies:
+                <br />
+                Blockchain Platform: Ethereum for smart contract deployment. Wallet
+                Integration: MetaMask for user authentication and transactions. Ether.js:
+                For interaction between the DApp and the blockchain. Frontend Framework:
+                Next.js for the user interface. Smart Contract Language: Solidity. Token
+                Standard: ERC-20 for governance tokens.
+                <br />
+                Milestones:
+                <br />
+                1. Project Initiation (1 week: Nov 4 - 10): Define project scope and
+                goals. Assemble the development team. Select the blockchain platform and
+                technologies.
+                <br />
+                2. DAO Creation and Membership Management (1 week: Nov 11 - 18 ): Develop
+                DAO creation functionality. Implement member management, allowing users to
+                join or leave DAOs.
+                <br />
+                3. User Authentication and Wallet Integration (1 week: ):
+                <br />
+                Secure user authentication with MetaMask. Enable seamless wallet
+                integration for transactions.
+                <br />
+                4. Proposal Management (1 week):
+                <br />
+                Implement a basic system for users to submit proposals. Allow for proposal
+                tracking and visibility.
+                <br />
+                5. Voting System (1 week): Create a simple, transparent voting mechanism.
+                Implement a basic majority voting system.
+                <br />
+                6. Smart Contract Development (1 week): Develop and deploy smart contracts
+                for DAO governance. Ensure basic security and auditability.
+                <br />
+                7. Token Integration (1 week): Create or integrate a governance token
+                (ERC-20). Implement basic token transfer functionality.
+                <br />
+                8. UI/UX Design (1 week):
+                <br />
+                Design a basic user interface for managing DAO memberships and voting
+                history. Focus on usability and simplicity.
+                <br />
+                9. Testing and Bug Fixing (1 week): Thoroughly test the DApp to identify
+                and fix critical bugs. Conduct limited user testing.
+                <br />
+                10. Deployment (1 week): Deploy the DApp on a testnet for initial testing.
+                Prepare for mainnet deployment.
+                <br />
+                11. Documentation (1 week): Provide minimal documentation for users and
+                developers. Include essential user guides and contract descriptions.
+                <br />
+                12. Compliance and Legal Assessment (1 week): Ensure basic compliance with
+                local regulations and legal requirements.
+                <br />
+                13. Maintenance/Bug fixing: Maintenance through Smart contract
+                upgradability.
+                <br />
+                Signed
+                <br />Favor Lawrence
+                <br />Somtochukwu Kelechi Onwuajuese
+                <br />Franklin Ugwuoke
+                <br />
+            </p>
+        </>
     );
 }
